49322050c046160a254b13fa22f60e99805f096a
PlacidaMouni <61835272+PlacidaMouni@users.noreply.github.com>
2024-10-27 13:36:58 +0100

    Implémentation de base de Grid

---
+++ src/main/java/fr/univ_amu/m1info/board_game_library/Grid.java
@@ -0,0 +1,94 @@
+package fr.univ_amu.m1info.board_game_library;
+
+public class Grid {
+    // Tableau 2D de Piece représentant le plateau de jeu 8x8
+    private Pion[][] squares;
+
+    // Constructeur
+    public Grid() {
+        squares = new Pion[8][8];
+        initializeGrid();
+    }
+
+    /**
+     * Initialise la grille de jeu pour une partie d'Othello
+     * Place les 4 pions de départ au centre du plateau
+     */
+    public void initializeGrid() {
+        // On s'assure que la grille est vide
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                squares[i][j] = null;
+            }
+        }
+
+        // Place les 4 pions de départ au centre
+        // Pions blancs
+        squares[3][3] = new Pion(Color.WHITE);
+        squares[4][4] = new Pion(Color.WHITE);
+        // Pions noirs
+        squares[3][4] = new Pion(Color.BLACK);
+        squares[4][3] = new Pion(Color.BLACK);
+    }
+
+    /**
+     * Vérifie si un mouvement est valide aux coordonnées données
+     * @param x coordonnée x sur la grille
+     * @param y coordonnée y sur la grille
+     * @return true si le mouvement est valide, false sinon
+     */
+    public boolean isValidMove(int x, int y) {
+        // Vérifie si les coordonnées sont dans la grille
+        if (x < 0 || x >= 8 || y < 0 || y >= 8) {
+            return false;
+        }
+
+        // Vérifie si la case est déjà occupée
+        return squares[x][y] == null;
+    }
+
+    /**
+     * Place un pion aux coordonnées spécifiées
+     * @param x coordonnée x sur la grille
+     * @param y coordonnée y sur la grille
+     * @param pion le pion à placer
+     */
+    public void placePion(int x, int y, Pion pion) {
+        if (isValidMove(x, y)) {
+            squares[x][y] = pion;
+        }
+    }
+
+    public void flipPion(int x, int y) {
+        if (squares[x][y] != null) {
+            Pion pion = (Pion) squares[x][y];
+            // Change la couleur du pion
+            if (pion.getColor() == Color.BLACK) {
+                pion.setColor(Color.WHITE);
+            } else {
+                pion.setColor(Color.BLACK);
+            }
+        }
+    }
+
+    public void displayGrid() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (squares[i][j] == null) {
+                    System.out.print("- ");
+                } else {
+                    Pion pion = (Pion) squares[i][j];
+                    System.out.print(pion.getColor() == Color.BLACK ? "B " : "W ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public Pion getPion(int x, int y) {
+        if (x >= 0 && x < 8 && y >= 0 && y < 8) {
+            return squares[x][y];
+        }
+        return null;
+    }
+}
