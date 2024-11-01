8565f7c67d355bc00cd20c8db8d899c37ebe6d4d
aristidecod <sie.ouattara@etud.univ_amu.fr>
2024-11-01 20:16:52 +0100

    refactor de la class TestPlayer

--- src/test/java/fr/univ_amu/m1info/board_game_library/TestPlayer.java
+++ src/test/java/fr/univ_amu/m1info/board_game_library/TestPlayer.java
@@ -6,38 +6,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestPlayer {
 
-    private Player player;
-    private Grid grid;
-
-    @BeforeEach
-    public void setUp() {
-        player = new Player("Player 1", Color.BLACK);
-        grid = new Grid();
-    }
-
-    @Test
-    public void testGetName() {
-        assertEquals("Player 1", player.getName(), "Le nom du joueur doit être 'Player 1'");
-    }
-
-    @Test
-    public void testGetColor() {
-        assertEquals(Color.BLACK, player.getColor(), "La couleur du joueur doit être noire (BLACK)");
-    }
-
-    @Test
-    public void testInitialScore() {
-        assertEquals(0, player.getScore(), "Le score initial du joueur doit être 0");
-    }
-
-    @Test
-    public void testSetScore() {
-        player.setScore(5);
-        assertEquals(5, player.getScore(), "Le score du joueur doit être mis à jour à 5");
-    }
-
     @Test
     public void testPlayValidMove() {
+        Player player;
+        Grid grid;
+
+        player = new Player("Player 1", Color.BLACK);
+        grid = new Grid();
+
         boolean result = player.play(5, 7, grid);
         // Vérifie que le coup a été joué et que le pion a été placé
         assertTrue(result, "Le coup doit être valide");
@@ -46,13 +22,25 @@ public class TestPlayer {
 
     @Test
     public void testPlayInvalidMove() {
+        Player player;
+        Grid grid;
+
+        player = new Player("Player 1", Color.BLACK);
+        grid = new Grid();
+
         boolean result = player.play(9, 9, grid);
         // Vérifie que le coup n'a pas été joué et qu'aucun pion n'a été placé
         assertFalse(result, "Le coup doit être invalide");
     }
 
     @Test
-    public void testCalculateScoreAndPercentage() {
+    public void testCalculateScore() {
+        Player player;
+        Grid grid;
+
+        player = new Player("Player 1", Color.BLACK);
+        grid = new Grid();
+
         // Le joueur joue plusieurs coups
         player.play(0, 0, grid);
         player.play(1, 0, grid);
@@ -66,5 +54,4 @@ public class TestPlayer {
         player.calculateScore(grid);
         assertEquals(5, player.getScore(), "Le score doit être calculé en fonction du nombre de pions de la couleur du joueur sachant qu'il y'a deja deux pions placer");
     }
-
 }
