8151d6549ae0fe581c85eec4518311c940208afc
aristidecod <sie.ouattara@etud.univ_amu.fr>
2024-11-06 16:48:52 +0100

    merge de la branch test/valid_move_placement dans la branch dev

--- src/test/java/fr/univ_amu/m1info/board_game_library/TestGrid.java
+++ src/test/java/fr/univ_amu/m1info/board_game_library/TestGrid.java
@@ -37,4 +37,27 @@ public class TestGrid {
             }
         }
     }
+
+    @Test
+    public void placePion_validMove() {
+        Grid grid = new Grid();
+        Pion pion = new Pion(Color.BLACK);
+
+        //suppoons que le pion noir est placé sur la case 5,4
+        boolean isPionPlaced = grid.placePion(2, 3, pion);
+
+        assertTrue(isPionPlaced, "The pion should be placed on the grid as the move is valid.");
+        assertEquals(pion, grid.getPion(2, 3), "The placed pion should be retrieved from the grid.");
+    }
+
+    @Test
+    public void placePion_invalidMove() {
+        Grid grid = new Grid();
+        Pion pion = new Pion(Color.BLACK);
+
+        boolean isPionPlaced = grid.placePion(0, 0, pion);
+
+        assertFalse(isPionPlaced, "The pion should not be placed on the grid as the move is invalid.");
+        assertEquals(null, grid.getPion(0, 0), "The grid at the specified point should be null as pion was not placed.");
+    }
 }
